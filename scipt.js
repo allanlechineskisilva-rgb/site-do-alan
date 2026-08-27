a3095933a5d8510d9f110a058d735974e196bdf6
allanlechineskisilva-rgb <allan.lechineski.silva@escola.pr.gov.br>
2026-08-27 12:01:50 -0300

    ok

---
+++ scipt.js
@@ -0,0 +1,11 @@
+
+<button onclick="irParaSite()">Ir para o site</button>
+
+<script>
+    function irParaSite() {
+        window.location.href = "https://pt.wikipedia.org/wiki/Honda_CG_160";
+    }
+</script>
+<a href="https://pt.wikipedia.org/wiki/Honda_CG_160" target="_blank" class="botao">
+    Conheça mais sobre a Honda 160
+</a>
